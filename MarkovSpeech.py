4b37752d863d09cc01b20ed3f19e4c93ce177444
Layne Kocer <lkocer@nevada.unr.edu>
2018-05-14 15:20:12 -0700

    fix pair_counts init in update_pair_count

--- MarkovSpeech.py
+++ MarkovSpeech.py
@@ -14,15 +14,24 @@ class MarkovSpeech:
         pass
 
     def update_pair_count(self, word1, word2):
+        if None in [word1, word2]:
+            return
+
         if word1 in self.word_counts:
             self.word_counts[word1] += 1
         else:
             self.word_counts[word1] = 1
 
-        if word2 in self.pair_counts[word1]:
-            self.pair_counts[word1][word2] += 1
+        if word1 not in self.pair_counts:
+            self.pair_counts[word1] = {}
+
+        if word2 in x[word1]:
+            x[word1][word2] += 1
         else:
-            self.pair_counts[word1][word2] = 1
+            x[word1][word2] = 1
+
+    def normalize_pair_count(self):
+
 
 
 if __name__ == "__main__":
