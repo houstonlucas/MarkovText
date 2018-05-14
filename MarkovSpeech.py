e7299dc2df2d573557032d002a57749b54f8f37d
houstonlucas <houstonlucas@nevada.unr.edu>
2018-05-14 14:39:27 -0700

    Mocked up quick skeleton.

---
+++ MarkovSpeech.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+
+def main():
+    ms = MarkovSpeech()
+
+
+class MarkovSpeech:
+    def __init__(self):
+        pair_counts = {}
+        word_counts = {}
+
+    def count_corpus(self, corpus):
+        pass
+
+    def update_pair_count(self, word1, word2):
+        pass
+
+
+if __name__ == "__main__":
+    main()
+
