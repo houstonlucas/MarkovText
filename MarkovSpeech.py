3f1b3ec9f6f63a58f037c77f1366980c8ef3dc91
Layne Kocer <lkocer@nevada.unr.edu>
2018-05-14 15:04:05 -0700

    made update_pair_count

--- MarkovSpeech.py
+++ MarkovSpeech.py
@@ -7,14 +7,22 @@ def main():
 
 class MarkovSpeech:
     def __init__(self):
-        pair_counts = {}
-        word_counts = {}
+        self.pair_counts = {}
+        self.word_counts = {}
 
     def count_corpus(self, corpus):
         pass
 
     def update_pair_count(self, word1, word2):
-        pass
+        if word1 in word_counts:
+            self.word_counts[word1] += 1
+        else:
+            self.word_counts[word1] = 1
+
+        if word2 in self.pair_counts[word1]:
+            self.pair_counts[word1][word2] += 1
+        else:
+            self.pair_counts[word1][word2] = 1
 
 
 if __name__ == "__main__":
